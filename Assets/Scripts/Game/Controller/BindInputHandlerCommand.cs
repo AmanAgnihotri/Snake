0d37d79df86be7d180245ce4922474fb809e7ac4
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:37:36 +0530

    Add BindInputHandlerCommand
    This command binds the right input handler with to the IInputHandler
    interface that can then be injected where its required.
    
    If the player wants to control the snake itself, either the
    KeyboardHandler or the SwipeHandler is bound, based on the device.
    
    If its the AI, then AIHandler is bound to the IInputHandler along with
    the right kind of pathfinder.

---
+++ Assets/Scripts/Game/Controller/BindInputHandlerCommand.cs
@@ -0,0 +1,39 @@
+﻿using strange.extensions.command.impl;
+
+namespace Snake
+{
+  public class BindInputHandlerCommand : Command
+  {
+    [Inject]
+    public InputHandlerType InputHandlerType { get; set; }
+
+    public override void Execute ()
+    {
+      injectionBinder.Unbind<IInputHandler> ();
+
+      injectionBinder.Unbind<IPathfinder> ();
+
+      if (InputHandlerType == InputHandlerType.Player)
+      {
+        #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+        injectionBinder.Bind<IInputHandler> ().To<KeyboardHandler> ().ToSingleton ();
+        #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_WP8_1
+        injectionBinder.Bind<IInputHandler> ().To<SwipeHandler> ().ToSingleton ();
+        #endif
+      }
+      else
+      {
+        injectionBinder.Bind<IInputHandler> ().To<AIHandler> ().ToSingleton ();
+
+        if (InputHandlerType == InputHandlerType.BoundedAI)
+        {
+          injectionBinder.Bind<IPathfinder> ().To<BoundedPathfinder> ().ToSingleton ();
+        }
+        else
+        {
+          injectionBinder.Bind<IPathfinder> ().To<UnboundedPathfinder> ().ToSingleton ();
+        }
+      }
+    }
+  }
+}
