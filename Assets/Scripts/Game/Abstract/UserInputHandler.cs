842c3eccd5bdfbf477f984437ba18dc30c7a092c
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:22:09 +0530

    Add UserInputHandler
    This class allows for handling of input by the user by deferring the
    HandleInput method to the implementing handlers, while implementing the
    input handling routine itself.

---
+++ Assets/Scripts/Game/Abstract/UserInputHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace Snake
+{
+  public abstract class UserInputHandler : AbstractInputHandler
+  {
+    public override IEnumerator Update ()
+    {
+      while (!Game.IsOver)
+      {
+        HandleInput ();
+
+        yield return null;
+      }
+    }
+
+    public abstract void HandleInput ();
+  }
+}
