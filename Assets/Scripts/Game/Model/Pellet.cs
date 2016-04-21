0d051a5492e1881fd575f0732f18398357a33438
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:26:25 +0530

    Add Pellet
    The Update method places a pellet on one of the available cells on the
    grid that aren’t occupied by anything else.

---
+++ Assets/Scripts/Game/Model/Pellet.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace Snake
+{
+  public class Pellet : IPellet
+  {
+    [Inject]
+    public IGrid Grid { get; set; }
+
+    public Cell Cell { get; private set; }
+
+    private Color color;
+
+    private int availableCellsCount;
+
+    private Cell[] availableCells;
+
+    private int minRow, maxRow, minColumn, maxColumn;
+
+    private static readonly System.Random Random = new System.Random ();
+
+    public Pellet (Color color, int rowInset, int columnInset)
+    {
+      this.color = color;
+
+      minRow = rowInset;
+
+      minColumn = columnInset;
+    }
+
+    [PostConstruct]
+    public void Initialize ()
+    {
+      maxRow = Grid.Rows - minRow;
+      maxColumn = Grid.Columns - minColumn;
+
+      availableCellsCount = (maxRow - minRow) * (maxColumn - minColumn);
+
+      availableCells = new Cell[availableCellsCount];
+    }
+
+    public void Update ()
+    {
+      UpdateAvailableCells ();
+
+      Cell = availableCells [Random.Next (availableCellsCount)];
+
+      Cell.SetActive (true, color);
+    }
+
+    private void UpdateAvailableCells ()
+    {
+      availableCellsCount = 0;
+
+      for (int row = minRow; row < maxRow; row++)
+      {
+        for (int column = minColumn; column < maxColumn; column++)
+        {
+          var cell = Grid.Cells[row, column];
+
+          if (!cell.IsActive ())
+          {
+            availableCells[availableCellsCount++] = cell;
+          }
+        }
+      }
+    }
+  }
+}
