61f0e81e09f9a3e4d428062aff9101e29fe4efda
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:22:56 +0530

    Add KeyboardHandler
    This handler handles the WASD keys of the keyboard and associates it
    with the four directions along which the snake can move.

---
+++ Assets/Scripts/Game/Model/InputHandler/KeyboardHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Snake
+{
+  public class KeyboardHandler : UserInputHandler
+  {
+    public override void HandleInput ()
+    {
+      switch (Input.inputString.ToUpper ())
+      {
+        case "D": Handle (Direction.Right); break;
+        case "S": Handle (Direction.Down); break;
+        case "A": Handle (Direction.Left); break;
+        case "W": Handle (Direction.Up); break;
+      }
+    }
+  }
+}
