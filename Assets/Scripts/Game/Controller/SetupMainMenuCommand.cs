cbdf5b764a93a281deb099dd91fe20ccc1d70efb
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:39:32 +0530

    Add SetupMainMenuCommand
    This command sets up the high score in the MainMenu if its greater than
    zero.

---
+++ Assets/Scripts/Game/Controller/SetupMainMenuCommand.cs
@@ -0,0 +1,23 @@
+﻿using strange.extensions.command.impl;
+
+namespace Snake
+{
+  public class SetupMainMenuCommand : Command
+  {
+    [Inject]
+    public SetHighScoreSignal SetHighScoreSignal { get; set; }
+
+    [Inject]
+    public IHighScoreService HighScoreService { get; set; }
+
+    public override void Execute ()
+    {
+      var highScore = HighScoreService.GetHighScore ();
+
+      if (highScore > 0)
+      {
+        SetHighScoreSignal.Dispatch (highScore);
+      }
+    }
+  }
+}
