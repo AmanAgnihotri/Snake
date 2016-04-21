187d60a26e061c10fc751167795721f00a799e39
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:41:29 +0530

    Add MainMenu
    The view represents the buttons and HighScore text of the screen. When
    a button is clicked, the signals are dispatched to its mediator, which
    then handles the further processing.

---
+++ Assets/Scripts/Game/View/MainMenuMediator.cs
@@ -0,0 +1,50 @@
+﻿using strange.extensions.mediation.impl;
+
+namespace Snake
+{
+  [Mediates (typeof (MainMenuView))]
+  public class MainMenuMediator : Mediator
+  {
+    [Inject]
+    public MainMenuView MainMenuView { get; set; }
+
+    [Inject]
+    public SetHighScoreSignal SetHighScoreSignal { get; set; }
+
+    [Inject]
+    public BindInputHandlerSignal BindInputHandlerSignal { get; set; }
+
+    [Inject]
+    public ShowSignal ShowSignal { get; set; }
+
+    public override void OnRegister ()
+    {
+      MainMenuView.HighScore.gameObject.SetActive (false);
+
+      MainMenuView.Signal.AddListener (SetupGame);
+
+      SetHighScoreSignal.AddListener (SetHighScore);
+    }
+
+    public override void OnRemove ()
+    {
+      MainMenuView.Signal.RemoveListener (SetupGame);
+
+      SetHighScoreSignal.RemoveListener (SetHighScore);
+    }
+
+    private void SetupGame (InputHandlerType inputHandlerType)
+    {
+      BindInputHandlerSignal.Dispatch (inputHandlerType);
+
+      ShowSignal.Dispatch (ShowType.InGame);
+    }
+
+    private void SetHighScore (int score)
+    {
+      MainMenuView.HighScore.gameObject.SetActive (true);
+
+      MainMenuView.HighScore.text = string.Format ("Highscore: {0:D4}", score);
+    }
+  }
+}
---
+++ Assets/Scripts/Game/View/MainMenuView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UI;
+using strange.extensions.signal.impl;
+using strange.extensions.mediation.impl;
+
+namespace Snake
+{
+  public class MainMenuView : View
+  {
+    public Text HighScore;
+
+    internal Signal<InputHandlerType> Signal = new Signal<InputHandlerType> ();
+
+    public void Play ()
+    {
+      Signal.Dispatch (InputHandlerType.Player);
+    }
+
+    public void PlayBoundedAI ()
+    {
+      Signal.Dispatch (InputHandlerType.BoundedAI);
+    }
+
+    public void PlayUnboundedAI ()
+    {
+      Signal.Dispatch (InputHandlerType.UnboundedAI);
+    }
+  }
+}
