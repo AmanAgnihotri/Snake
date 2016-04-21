0e37c04e8cb688ee1f4ea092240f0ff65df8acb4
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:46:45 +0530

    Add GameBootstrap
    Creates the game’s context which then binds the signals to commands and
    invokes the commands that then do further processing and start the game.

---
+++ Assets/Scripts/Game/GameBootstrap.cs
@@ -0,0 +1,12 @@
+﻿using strange.extensions.context.impl;
+
+namespace Snake
+{
+  public class GameBootstrap : ContextView
+  {
+    private void Awake ()
+    {
+      context = new GameContext (this);
+    }
+  }
+}
