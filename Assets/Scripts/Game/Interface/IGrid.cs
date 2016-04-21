71e0278166c801a9c7b4c679ecd93a38511f6be5
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:20:03 +0530

    Add IGrid
    Represents the interface for the grid in the game that is made of cells.

---
+++ Assets/Scripts/Game/Interface/IGrid.cs
@@ -0,0 +1,13 @@
+﻿namespace Snake
+{
+  public interface IGrid
+  {
+    int Rows { get; }
+
+    int Columns { get; }
+
+    Cell[,] Cells { get; }
+
+    void Validate (ref int row, ref int column);
+  }
+}
