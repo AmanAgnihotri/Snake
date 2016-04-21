a20a743ed2b9d4e9eab4165b1b066714c3697ae0
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:01:58 +0530

    Add Position
    Used as a data container to move to neighbouring cells on the grid.

---
+++ Assets/Scripts/Game/Data Containers/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace Snake
+{
+  public class Position
+  {
+    public int Row { get; private set; }
+
+    public int Column { get; private set; }
+
+    public Position (int row, int column)
+    {
+      Row = row;
+
+      Column = column;
+    }
+  }
+}
