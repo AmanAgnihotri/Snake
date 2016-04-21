9d774adacb233f6b842b6bd88053324753d5494f
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:31:13 +0530

    Add ISnake
    Interface for the snake in the game.

---
+++ Assets/Scripts/Game/Interface/ISnake.cs
@@ -0,0 +1,21 @@
+﻿namespace Snake
+{
+  public interface ISnake
+  {
+    Direction CurrentDirection { get; }
+
+    Direction NextDirection { get; set; }
+
+    Cell Head { get; }
+
+    void Initialize ();
+
+    Cell GetNextCell ();
+
+    void AddHead (Cell cell);
+
+    void RemoveTail ();
+
+    void Deactivate ();
+  }
+}
