008a823ea9737b7a475db02f6f7280caf273ce12
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:36:11 +0530

    Add GameSignals
    The various signals that are dispatched in the game.

---
+++ Assets/Scripts/Game/Config/GameSignals.cs
@@ -0,0 +1,22 @@
+﻿using strange.extensions.signal.impl;
+
+namespace Snake
+{
+  public class BindDataSignal : Signal {}
+
+  public class BindInputHandlerSignal : Signal<InputHandlerType> {}
+
+  public class ShowSignal : Signal<ShowType> {}
+
+  public class SetupMainMenuSignal : Signal {}
+
+  public class UpdateScoreSignal : Signal<int> {}
+
+  public class StartGameSignal : Signal {}
+
+  public class SetHighScoreSignal : Signal<int> {}
+
+  public class GameOverSignal : Signal<bool, int> {}
+
+  public class SetupAfterGameSignal : Signal<bool, int> {}
+}
