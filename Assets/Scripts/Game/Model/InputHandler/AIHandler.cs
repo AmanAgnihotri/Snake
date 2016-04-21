aace8b802dc1c4fa73ce9fb55f863486f373a0d1
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:34:25 +0530

    Add AIHandler
    This input handler is managed by the AI, as it uses the pathfinder to
    make the snake move on the grid.

---
+++ Assets/Scripts/Game/Model/InputHandler/AIHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Snake
+{
+  public class AIHandler : AbstractInputHandler
+  {
+    [Inject]
+    public IPellet Pellet { get; set; }
+
+    [Inject]
+    public IPathfinder Pathfinder { get; set; }
+
+    private Cell snakeHead;
+
+    public override IEnumerator Update ()
+    {
+      while (!Game.IsOver)
+      {
+        snakeHead = Snake.Head;
+
+        var directions = Pathfinder.GetDirections (snakeHead, Pellet.Cell);
+
+        var isPathAvailable = false;
+
+        foreach (var dir in directions)
+        {
+          isPathAvailable = true;
+
+          Handle (dir);
+
+          while (snakeHead == Snake.Head)
+          {
+            yield return new WaitForSeconds (Game.TimeSkipPerFrame);
+          }
+
+          snakeHead = Snake.Head;
+        }
+
+        if (!isPathAvailable)
+        {
+          while (snakeHead == Snake.Head)
+          {
+            yield return new WaitForSeconds (Game.TimeSkipPerFrame);
+          }
+        }
+      }
+    }
+  }
+}
