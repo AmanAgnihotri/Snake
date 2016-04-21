074b999e4ed8aabee47eaae9021f16549c072e12
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:38:38 +0530

    Add HighScoreService
    This service saves and loads the high score in the game.

---
+++ Assets/Scripts/Game/Service/HighScoreService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Snake
+{
+  [Implements (typeof (IHighScoreService))]
+  public class HighScoreService : IHighScoreService
+  {
+    private int highScore = 0;
+
+    private string filePath = Application.persistentDataPath + "/" + "HighScore";
+
+    public int GetHighScore ()
+    {
+      if (File.Exists(filePath) && highScore <= 0)
+      {
+        LoadHighScore ();
+      }
+
+      return highScore;
+    }
+
+    public void Persist (int score)
+    {
+      if (score > highScore)
+      {
+        highScore = score;
+      }
+
+      File.WriteAllText (filePath, string.Format ("{0}", score));
+    }
+
+    private void LoadHighScore ()
+    {
+      Int32.TryParse (File.ReadAllText(filePath), out highScore);
+    }
+  }
+}
