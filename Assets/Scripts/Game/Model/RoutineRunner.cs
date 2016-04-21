eb044e188f58e4e96e34ffbad4b9d112ae9940dc
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 00:08:16 +0530

    Add RoutineRunner
    The implementation of IRoutineRunner that allows objects to invoke
    coroutines.

---
+++ Assets/Scripts/Game/Model/RoutineRunner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.context.api;
+using strange.extensions.injector.api;
+
+namespace Snake
+{
+  [Implements (typeof (IRoutineRunner), InjectionBindingScope.CROSS_CONTEXT)]
+  public class RoutineRunner : IRoutineRunner
+  {
+    [Inject (ContextKeys.CONTEXT_VIEW)]
+    public GameObject ContextView { get; set; }
+
+    private RoutineRunnerBehaviour Runner { get; set; }
+
+    [PostConstruct]
+    public void PostConstruct ()
+    {
+      Runner = ContextView.AddComponent<RoutineRunnerBehaviour> ();
+    }
+
+    public Coroutine StartRoutine (IEnumerator routine)
+    {
+      return Runner.StartCoroutine (routine);
+    }
+
+    public void StopAllRoutines ()
+    {
+      Runner.StopAllCoroutines ();
+    }
+
+    private class RoutineRunnerBehaviour : MonoBehaviour {}
+  }
+}
