1f987b4661d5284596b15beaf23d412697d388ef
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:43:16 +0530

    Add AfterGame
    This view shows the result of the game after its over, and allows for a
    restart and going back to the main menu.

---
+++ Assets/Scripts/Game/View/AfterGameMediator.cs
@@ -0,0 +1,43 @@
+﻿using strange.extensions.mediation.impl;
+
+namespace Snake
+{
+  [Mediates (typeof (AfterGameView))]
+  public class AfterGameMediator : Mediator
+  {
+    [Inject]
+    public AfterGameView AfterGameView { get; set; }
+
+    [Inject]
+    public ShowSignal ShowSignal { get; set; }
+
+    [Inject]
+    public SetupAfterGameSignal SetupAfterGameSignal { get; set; }
+
+    public override void OnRegister ()
+    {
+      AfterGameView.Signal.AddListener (HandleType);
+
+      SetupAfterGameSignal.AddListener (SetupValues);
+    }
+
+    public override void OnRemove ()
+    {
+      AfterGameView.Signal.RemoveListener (HandleType);
+
+      SetupAfterGameSignal.RemoveListener (SetupValues);
+    }
+
+    private void HandleType (ShowType type)
+    {
+      ShowSignal.Dispatch (type);
+    }
+
+    private void SetupValues (bool hasWon, int score)
+    {
+      AfterGameView.State.text = hasWon ? "Game Won!" : "Game Over!";
+
+      AfterGameView.Score.text = string.Format ("Score: {0:D4}", score);
+    }
+  }
+}
---
+++ Assets/Scripts/Game/View/AfterGameView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+using strange.extensions.signal.impl;
+using strange.extensions.mediation.impl;
+
+namespace Snake
+{
+  public class AfterGameView : View
+  {
+    public Text State;
+
+    public Text Score;
+
+    internal Signal<ShowType> Signal = new Signal<ShowType> ();
+
+    public void Restart ()
+    {
+      Signal.Dispatch (ShowType.InGame);
+    }
+
+    public void GotoMainMenu ()
+    {
+      Signal.Dispatch (ShowType.MainMenu);
+    }
+  }
+}
