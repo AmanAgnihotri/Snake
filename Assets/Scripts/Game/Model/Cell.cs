d14eff744b1e99523f25194cd670404188cb22fe
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-21 23:54:03 +0530

    Add Cell
    Represents the Cell in the form of a model. Knows about the row and
    column in which its present on the grid, and has methods to make it
    active and change its colour.

---
+++ Assets/Scripts/Game/Model/Cell.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Snake
+{
+  public class Cell
+  {
+    public int Row { get; private set; }
+
+    public int Column { get; private set; }
+
+    private GameObject gameObject;
+
+    private SpriteRenderer renderer;
+
+    public Cell (int row, int column, GameObject gameObject)
+    {
+      Row = row;
+
+      Column = column;
+
+      this.gameObject = gameObject;
+
+      renderer = gameObject.GetComponent<SpriteRenderer> ();
+    }
+
+    public bool IsActive ()
+    {
+      return gameObject.activeSelf;
+    }
+
+    public void SetActive (bool value)
+    {
+      gameObject.SetActive (value);
+    }
+
+    public void SetActive (bool value, Color color)
+    {
+      gameObject.SetActive (value);
+
+      renderer.color = color;
+    }
+  }
+}
