815731c15b7a836ee3b46b2d9cc937098ad07a0f
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-21 23:55:19 +0530

    Add DataConfig
    Represents the various configurable properties of the game elements.
    
    The not so apparent configurable properties are:
    
    1. RowInset and ColumnInset in PelletConfig: A Grid with 4 rows and 4
    columns and value 1 to both RowInset and ColumInset would leave the
    center 2x2 grid for pellets to become active at, as the insets would
    work as the border to the grid.
    
    2. TimeSkipPerFrame: This represents how quickly the snake moves. The
    value is in seconds.

---
+++ Assets/Scripts/Game/Data Containers/DataConfig.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Snake
+{
+  public class PelletConfig
+  {
+    public Color Color { get; set; }
+
+    public int RowInset { get; set; }
+
+    public int ColumnInset { get; set; }
+  }
+
+  public class SnakeConfig
+  {
+    public Color HeadColor { get; set; }
+
+    public Color BodyColor { get; set; }
+
+    public int InitialSize { get; set; }
+  }
+
+  public class GridConfig
+  {
+    public int Rows { get; set; }
+
+    public int Columns { get; set; }
+  }
+
+  public class DataConfig
+  {
+    public Color BackgroundColor { get; set; }
+
+    public PelletConfig Pellet { get; set; }
+
+    public SnakeConfig Snake { get; set; }
+
+    public GridConfig Grid { get; set; }
+
+    public float TimeSkipPerFrame { get; set; }
+  }
+}
