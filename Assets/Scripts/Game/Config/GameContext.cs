5afd4a94a9884a9c87a5148fc4ddc47fe76d1721
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:45:56 +0530

    Add GameContext
    This context binds the various signals to commands and binds the
    signals for dependency injection.

---
+++ Assets/Scripts/Game/Config/GameContext.cs
@@ -0,0 +1,34 @@
+﻿using strange.extensions.context.impl;
+
+namespace Snake
+{
+  public class GameContext : MVCSContext
+  {
+    public GameContext (ContextView contextView) : base (contextView) {}
+
+    protected override void mapBindings ()
+    {
+      implicitBinder.ScanForAnnotatedClasses ("Snake");
+
+      injectionBinder.Bind<ShowSignal> ().ToSingleton ();
+      injectionBinder.Bind<UpdateScoreSignal> ().ToSingleton ();
+      injectionBinder.Bind<SetHighScoreSignal> ().ToSingleton ();
+      injectionBinder.Bind<SetupAfterGameSignal> ().ToSingleton ();
+
+      commandBinder.Bind<BindDataSignal> ().To<BindDataCommand> ().Once ();
+      commandBinder.Bind<BindInputHandlerSignal> ().To<BindInputHandlerCommand> ().Pooled ();
+
+      commandBinder.Bind<SetupMainMenuSignal> ().To<SetupMainMenuCommand> ().Pooled ();
+
+      commandBinder.Bind<StartGameSignal> ().To<StartGameCommand> ().Pooled ();
+      commandBinder.Bind<GameOverSignal> ().To<GameOverCommand> ().Pooled ();
+    }
+
+    public override void Launch ()
+    {
+      injectionBinder.GetInstance<BindDataSignal> ().Dispatch ();
+
+      injectionBinder.GetInstance<ShowSignal> ().Dispatch (ShowType.MainMenu);
+    }
+  }
+}
