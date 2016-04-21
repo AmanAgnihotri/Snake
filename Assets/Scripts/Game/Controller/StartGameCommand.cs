9a5fe97039ec073084ab23f40e914df55ca3aaa1
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:38:20 +0530

    Add StartGameCommand
    This command is invoked at the start of the game. This is the one that
    starts the game’s routines.

---
+++ Assets/Scripts/Game/Controller/StartGameCommand.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using strange.extensions.command.impl;
+
+namespace Snake
+{
+  public class StartGameCommand : Command
+  {
+    [Inject]
+    public IGame Game { get; set; }
+
+    [Inject]
+    public IInputHandler InputHandler { get; set; }
+
+    [Inject]
+    public IRoutineRunner RoutineRunner { get; set; }
+
+    [Inject]
+    public UpdateScoreSignal UpdateScoreSignal { get; set; }
+
+    public override void Execute ()
+    {
+      Camera.main.orthographicSize = Screen.height / 2f;
+
+      UpdateScoreSignal.Dispatch (0);
+
+      RoutineRunner.StartRoutine (Game.Update ());
+
+      RoutineRunner.StartRoutine (InputHandler.Update ());
+
+      RoutineRunner.StartRoutine (InputHandler.UpdateEscape ());
+    }
+  }
+}
