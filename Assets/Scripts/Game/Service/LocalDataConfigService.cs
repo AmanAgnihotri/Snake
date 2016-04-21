8c317798dd66764e884aadcbadc29b29c6216d89
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-21 23:59:45 +0530

    Add LocalDataConfigService
    This service implements the IDataConfigService, and loads the data
    config from the JSON file present locally in the game’s resources.

---
+++ Assets/Scripts/Game/Service/LocalDataConfigService.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using strange.extensions.promise.api;
+using strange.extensions.promise.impl;
+
+namespace Snake
+{
+  [Implements (typeof (IDataConfigService))]
+  public class LocalDataConfigService : IDataConfigService
+  {
+    private DataConfig dataConfig;
+
+    private IPromise<DataConfig> promise = new Promise<DataConfig> ();
+
+    public IPromise<DataConfig> GetDataConfig ()
+    {
+      if (dataConfig != null)
+      {
+        promise.Dispatch (dataConfig);
+      }
+      else
+      {
+        LoadDataConfig ();
+      }
+
+      return promise;
+    }
+
+    private void LoadDataConfig ()
+    {
+      var dataConfigJson = Resources.Load<TextAsset> ("DataConfig").text;
+
+      dataConfig = JsonConvert.DeserializeObject<DataConfig> (dataConfigJson);
+
+      promise.Dispatch (dataConfig);
+    }
+  }
+}
