0fead6231ff1725e2aad3e973dd6e163b785caf0
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:42:18 +0530

    Add InGame
    The InGame screen shows the score of the game as the snake moves on the
    grid. Its mediator keeps on updating the score accordingly.

---
+++ Assets/Scripts/Game/View/InGameMediator.cs
@@ -0,0 +1,29 @@
+﻿using strange.extensions.mediation.impl;
+
+namespace Snake
+{
+  [Mediates (typeof (InGameView))]
+  public class InGameMediator : Mediator
+  {
+    [Inject]
+    public InGameView InGameView { get; set; }
+
+    [Inject]
+    public UpdateScoreSignal UpdateScoreSignal { get; set; }
+
+    public override void OnRegister ()
+    {
+      UpdateScoreSignal.AddListener (UpdateScore);
+    }
+
+    public override void OnRemove ()
+    {
+      UpdateScoreSignal.RemoveListener (UpdateScore);
+    }
+
+    private void UpdateScore (int score)
+    {
+      InGameView.Score.text = string.Format ("Score: {0:D4}", score);
+    }
+  }
+}
---
+++ Assets/Scripts/Game/View/InGameView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.UI;
+using strange.extensions.mediation.impl;
+
+namespace Snake
+{
+  public class InGameView : View
+  {
+    public Text Score;
+  }
+}
