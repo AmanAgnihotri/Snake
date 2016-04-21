60a7a94f6b0abcb07b64f3b0cee21d9e586184e3
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:35:16 +0530

    Add BindDataCommand
    This command binds the values of various game elements to their
    interfaces, so that dependency injection can be applied on them,
    wherever those elements are required.
    
    It utilises the IDataConfigService and IHighScoreService to bind the
    configured values to the elements.

---
+++ Assets/Scripts/Game/Controller/BindDataCommand.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using strange.extensions.command.impl;
+
+namespace Snake
+{
+  public class BindDataCommand : Command
+  {
+    [Inject]
+    public IDataConfigService DataConfigService { get; set; }
+
+    [Inject]
+    public IHighScoreService HighScoreService { get; set; }
+
+    public override void Execute ()
+    {
+      DataConfigService.GetDataConfig ().Then (BindData);
+    }
+
+    private void BindData (DataConfig data)
+    {
+      Camera.main.backgroundColor = data.BackgroundColor;
+
+      BindGrid (data.Grid);
+
+      BindPellet (data.Pellet, data.Grid);
+
+      BindSnake (data.Snake);
+
+      BindGame (data);
+    }
+
+    private void BindGrid (GridConfig g)
+    {
+      var grid = new Grid (g.Rows, g.Columns);
+
+      injectionBinder.Bind<IGrid> ().ToValue (grid).ToSingleton ();
+    }
+
+    private void BindPellet (PelletConfig p, GridConfig g)
+    {
+      while ((p.RowInset << 1) >= g.Rows)
+      {
+        p.RowInset >>= 1;
+      }
+
+      while ((p.ColumnInset << 1) >= g.Columns)
+      {
+        p.ColumnInset >>= 1;
+      }
+
+      var pellet = new Pellet (p.Color, p.RowInset, p.ColumnInset);
+
+      injectionBinder.Bind<IPellet> ().ToValue (pellet).ToSingleton ();
+    }
+
+    private void BindSnake (SnakeConfig s)
+    {
+      var snake = new Snake (s.HeadColor, s.BodyColor, s.InitialSize);
+
+      injectionBinder.Bind<ISnake> ().ToValue (snake).ToSingleton ();
+    }
+
+    private void BindGame (DataConfig d)
+    {
+      var g = d.Grid;
+      var p = d.Pellet;
+
+      var maxScore = (g.Rows - (p.RowInset << 1)) * (g.Columns - (p.ColumnInset << 1));
+
+      var game = new Game (maxScore, d.TimeSkipPerFrame);
+
+      injectionBinder.Bind<IGame> ().ToValue (game).ToSingleton ();
+    }
+  }
+}
