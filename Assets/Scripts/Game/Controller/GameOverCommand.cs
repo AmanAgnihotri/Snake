1b9377180a0e484989ebbbb8db2ec6c3a72830e9
Aman Agnihotri <aman.agnihotri@yahoo.com>
2016-04-22 01:38:54 +0530

    Add GameOverCommand
    Invoked when the game is over.

---
+++ Assets/Scripts/Game/Controller/GameOverCommand.cs
@@ -0,0 +1,31 @@
+﻿using strange.extensions.command.impl;
+
+namespace Snake
+{
+  public class GameOverCommand : Command
+  {
+    [Inject]
+    public bool HasWon { get; set; }
+
+    [Inject]
+    public int Score { get; set; }
+
+    [Inject]
+    public IGame Game { get; set; }
+
+    [Inject]
+    public IRoutineRunner RoutineRunner { get; set; }
+
+    [Inject]
+    public IHighScoreService HighScoreService { get; set; }
+
+    public override void Execute ()
+    {
+      Game.Reset ();
+
+      RoutineRunner.StopAllRoutines ();
+
+      HighScoreService.Persist (Score);
+    }
+  }
+}
